08be7ccc10d18ecefd7987528d4828638ddbb64b
Guillermo Torres Pérez <60155590+GuilleTorresPerez@users.noreply.github.com>
2020-09-24 12:46:23 +0200

    Add files via upload

---
+++ gaus 2.0.py
@@ -0,0 +1,12 @@
+# -*- coding: cp1252 -*-
+def sumaGaus():
+    finalNumber=input ("Inserte el número por favor")
+    sumaAcumulada=0
+
+    for cont in range(1,finalNumber+1):
+        sumaAcumulada = sumaAcumulada+cont
+
+    print "La suma de los números hasta ",finalNumber
+    print "= ", sumaAcumulada
+
+sumaGaus()
