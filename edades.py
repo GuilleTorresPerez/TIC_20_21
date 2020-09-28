fcb6582e0c106713c819ba5016b5b2c168ccc5d0
Guillermo Torres Pérez <60155590+GuilleTorresPerez@users.noreply.github.com>
2020-09-28 17:21:13 +0200

    Add files via upload

--- edades.py
+++ edades.py
@@ -4,28 +4,31 @@
 #el mayor y el menor
 
 def edad_media():
-    edadMayor=0
-    edadMenor=0
-    diferencia=0
-    sumaEdades=0
     
-    for cont in range(1, 11):
-        print("Introduzca la edad del alumno "+cont)
+    print"Introduzca la edad del alumno 1"
+    Alumno = input()
+    edadMayor=Alumno
+    edadMenor=Alumno
+    sumaEdades=Alumno
+    
+    for cont in range(2, 11):
+        print"Introduzca la edad del alumno",cont
         Alumno = input()
         sumaEdades=sumaEdades+Alumno
-
         if Alumno > edadMayor:
             edadMayor=Alumno
         if Alumno < edadMenor:
             edadMenor=Alumno
 
+    diferencia=edadMayor-edadMenor
+    edadMedia=sumaEdades/(cont-1)
+
+    print "El número de alumnos es",cont
     print"La edad mayor es ", edadMayor
     print"La edad menor es ", edadMenor
-    diferencia=edadMayor-edadMenor
+    print"La diferencia entre la edad mayor y la edad menor es ",diferencia
     print"La suma de las edades es ", sumaEdades
+    print"La edad media es", edadMedia
 
 edad_media()
-        
-        
-        
         
