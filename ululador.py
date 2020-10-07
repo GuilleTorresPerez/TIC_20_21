b162bb6d76672f797bdf444f97165476cc80124c
Guillermo Torres Pérez <60155590+GuilleTorresPerez@users.noreply.github.com>
2020-10-07 09:28:06 +0200

    Add files via upload

---
+++ ululador.py
@@ -0,0 +1,22 @@
+#Haz un programa que lea una cadena y sustituya las vocales
+#por la letra u. palabra de 10
+
+def ululador():
+    a = 'a'
+    e = 'e'
+    i = 'i'
+    o = 'o'
+    u = 'u'
+    palabra=raw_input("Introduce la palabra ")
+    longitudPalabra =(len(palabra)) 
+    print "La longitud de la palabra es ",longitudPalabra
+
+    for cont in range(0,longitudPalabra):
+        if (palabra[cont] == a or palabra[cont] == e or palabra[cont] == i or palabra[cont] == o or palabra[cont] == u):
+            print 'u'
+        else:
+            print palabra[cont]
+
+
+ululador()
+        
